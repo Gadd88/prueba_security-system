51c2b4792d796322fc47d3f0353a7e46c18283e7
Matias Saade <matiassaade@gmail.com>
2024-07-05 00:41:05 -0300

    fix: solucionado error al correr test por config en tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,33 @@
-import type { Config } from 'jest';
+// import type { Config } from 'jest';
 
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-};
+// const config: Config = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jest-environment-jsdom',
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+//   moduleNameMapper: {
+//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+//   },
+//   transform: {
+//     '^.+\\.(ts|tsx)$': ['ts-jest', {
+//       tsconfig: 'tsconfig.jest.json'}]
+//   },
+//   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+// };
 
-export default config;
+// export default config;
+
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+/** @type {import('jest').Config} */
+const config = {
+    // Add more setup options before each test is run
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jest-environment-jsdom',
+    preset: 'ts-jest',
+}
+
+module.exports = createJestConfig(config)
